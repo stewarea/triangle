e7ef94856f0048ed237125a2ba3de9627025b2ca
evan stewart and patricia hall <patricialynnhall@gmail.com>
2016-02-24 20:34:00 -0800

    do everything

--- js/scripts.js
+++ js/scripts.js
@@ -1,44 +1,25 @@
-// var leapYear = function(year) {
-//   if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
-//     return true;
-//   } else {
-//     return false;
-//   }
-// };
-
+var tritype = function(side1, side2, side3) {
+  if ((side1 === side2) && (side1 === side3)) {
+      // $("#equil").show();
+      return "equilateral"
+} else if ((side1 === side2) || (side2 === side3) || (side1 === side3)) {
+      // $("#iso").show();
+      return "ioceles"
+} else if ((side1 + side2 > side3) || (side1 + side3 > side2) || (side2 + side3 > side1)) {
+      // $("#scalene").show();
+      return "scalene"
+} else   {
+      // $("#error").show();
+      return "this is not a triangle"
+  }
 
 $(document).ready(function() {
-  $("form#leap-year").submit(function(event) {
+  $("form#triangles").submit(function(event) {
     var side1 = parseInt($("input#side1").val());
     var side2 = parseInt($("input#side2").val());
     var side3 = parseInt($("input#side3").val());
-    var result = tritype(parameters);
-
-
-    var tritype = function() {
-      if ((side1 === side2) && (side1 === side3) && (side2 === side3))
-        $(form#tritype).show();
-      else if ((side1 != side2) && (side1 != side3) && (side2 != side3))
-        $(())
-    }
-
-    $(".year").text(year);
-
-
-
-    var result = leapYear(year);
-
-    if  {
-
-
-    } else if (result) {
-
-
-    } else {
-
-    }
-
-
+    var result = tritype(side1, side2, side3);
     event.preventDefault();
-  });
+  })
+};
 });
